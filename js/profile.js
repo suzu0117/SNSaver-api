b90b044515d1a09366d8c8313881572005d774b3
suzu0117 <142565665+suzu0117@users.noreply.github.com>
2026-08-03 07:21:16 +0900

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -8,11 +8,10 @@ export async function getProfile(username) {
         {
             headers: {
                 "x-ig-app-id": process.env.IG_APP_ID,
+                cookie: process.env.IG_COOKIE,
                 "user-agent": process.env.IG_USER_AGENT,
-                "referer": `https://www.instagram.com/${username}/`,
-                "accept": "application/json",
-                "accept-language": "ja,en-US;q=0.9,en;q=0.8",
-            }
+                referer: `https://www.instagram.com/${username}/`,
+            },
         },
     );
     
