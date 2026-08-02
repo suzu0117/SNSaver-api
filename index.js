77b68c80d340749df3f4b2ba7de183f332feea33
suzu0117 <142565665+suzu0117@users.noreply.github.com>
2026-08-03 05:23:08 +0900

    Refactor server setup and remove redundant code

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { router } from "./js/router.js";
-const app = express();
-import path from "path";
 
-app.use("/profileimage", (req, res, next) => {
-    console.log(req.url);
-    next();
-});
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
 app.use("/profileimage", express.static("profileimage"));
 app.use("/temp", express.static("temp"));
-app.use(express.json());
-app.use(cors());
 
 app.use("/api", router);
 
-app.listen(3000, () => {
-    console.log("Server listening on port 3000");
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
